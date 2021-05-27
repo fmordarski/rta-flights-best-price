ada3941df3e8bbb42c675af39eb954ebb079be5d
fmordarski <fmordarski@gmail.com>
2021-05-27 23:56:07 +0200

    collecting data and saving into csv

--- data.py
+++ data.py
@@ -1,10 +1,13 @@
-from utils import Skyscanner
+from utils import Skyscanner, Utils
 import argparse
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--apikey', type=str, help='apikey for Skyscanner')
-parser.add_argument('countries', metavar='N', type=str, nargs='+', help="countries for which we collect data")
+parser.add_argument('--countries', metavar='N', type=str, nargs='+',
+                    help="countries for which we collect data")
+parser.add_argument('--months', metavar='N', type=int, nargs='+',
+                    help="months for which we collect data")
 
 args = parser.parse_args()
 
@@ -12,8 +15,11 @@ base_url = "https://partners.api.skyscanner.net/apiservices"
 locale = "en-EN"
 country = "PL"
 currency = "PLN"
+output_folder = Utils.get_output_dir()
 
 
-api = Skyscanner(base_url, args.apikey, locale, country, currency, args.countries)
-locations = api.get_locations()
-print(locations)
+api = Skyscanner(base_url, args.apikey, locale, country, currency,
+                 args.countries, args.months)
+data = api.process()
+data = Utils.convert_to_pandas(data)
+Utils.save_csv(data, output_folder)
--- utils.py
+++ utils.py
@@ -1,22 +1,92 @@
 import requests
+from datetime import datetime
+import calendar
+from itertools import combinations, product
+import pandas as pd
+import os
 
 
 class Skyscanner:
 
-    def __init__(self, base_url, apikey, locale, country, currency, selected_countries):
+    def __init__(self, base_url, apikey, locale, country, currency,
+                 selected_countries, months):
         self.base_url = base_url
         self.apikey = apikey
         self.locale = locale
         self.country = country
         self.currency = currency
         self.selected_countries = selected_countries
+        self.months = months
+        self.days = []
         self.locations = []
+        self.quotes = self._quote_dict()
+
+    def process(self):
+        self.get_locations()
+        self.get_combinations()
+        self.get_dates()
+        self.get_all_quotes()
+        return self.quotes
+
+    def _quote_dict(self):
+        return {"Origin": [], "Destination": [], "Price": [], "Date": []}
 
     def get_locations(self):
+        url = f"{self.base_url}/autosuggest/v1.0/{self.country}/" + \
+              f"{self.currency}/{self.locale}"
         for country in self.selected_countries:
-            url = f"{self.base_url}/autosuggest/v1.0/{self.country}/{self.currency}/{self.locale}"
-            params = {"query": country, "apiKey": self.apikey, "includeCountries": "false"}
-            r = requests.get(url, params = params)
+            params = {"query": country, "apiKey": self.apikey,
+                      "includeCountries": "false"}
+            r = requests.get(url, params=params)
             for place in r.json()["Places"]:
                 self.locations.append(place["PlaceId"])
-        return self.locations
+
+    def get_combinations(self):
+        self.locations_pairs = [comb for comb in combinations(self.locations,
+                                                              2)]
+
+    def get_dates(self):
+        year = datetime.now().year
+        for month in self.months:
+            num_days = calendar.monthrange(year, month)[1]
+            days = [datetime.strptime(f"{year}-{month}-{day}", "%Y-%m-%d").
+                    strftime("%Y-%m-%d") for day in range(1, num_days+1)]
+            self.days += days
+
+    def get_all_quotes(self):
+        for pair, day in product(self.locations_pairs, self.days):
+            quotes = self.get_quote(pair[0], pair[1], day)
+            for quote in quotes:
+                self.append_quote(pair[0], pair[1], day, quote)
+            quotes = self.get_quote(pair[1], pair[0], day)
+            for quote in quotes:
+                self.append_quote(pair[1], pair[0], day, quote)
+
+    def append_quote(self, origin, dest, date, quote):
+        self.quotes["Origin"].append(origin)
+        self.quotes["Destination"].append(dest)
+        self.quotes["Price"].append(quote["MinPrice"])
+        self.quotes["Date"].append(date)
+
+    def get_quote(self, origin, dest, day):
+        url = f"{self.base_url}/browsequotes/v1.0/{self.country}/" + \
+              f"{self.currency}/{self.locale}/{origin}/{dest}/{day}/"
+        params = {"apiKey": self.apikey}
+        r = requests.get(url, params=params)
+        return r.json()["Quotes"]
+
+
+class Utils:
+
+    @staticmethod
+    def convert_to_pandas(dict):
+        return pd.DataFrame.from_dict(dict)
+
+    @staticmethod
+    def save_xlsx(df, path):
+        df.to_csv(path, index=False)
+
+    @staticmethod
+    def get_output_dir():
+        dir = os.path.dirname(__file__)
+        return os.path.join(dir, "output")
